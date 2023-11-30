eec604dc95aa9e6a27803ebeae62469e3b416d8c
Iurii Egorov <ye@amnezia.org>
2023-11-30 10:18:00 +0300

    Fix wg-quick

--- src/wg-quick/linux.bash
+++ src/wg-quick/linux.bash
@@ -87,7 +87,7 @@ auto_su() {
 
 add_if() {
 	local ret
-	if ! cmd ip link add "$INTERFACE" type amnezia; then
+	if ! cmd ip link add "$INTERFACE" type amneziawg; then
 		ret=$?
 		[[ -e /sys/module/amneziawg ]] || ! command -v "${WG_QUICK_USERSPACE_IMPLEMENTATION:-amneziawg-go}" >/dev/null && exit $ret
 		echo "[!] Missing WireGuard (Amnezia VPN) kernel module. Falling back to slow userspace implementation." >&2
